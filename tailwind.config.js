57ef4ae2516e291ddc2540ad339502fb32457f0b
Jerrylum <serverofjerry@gmail.com>
2024-06-05 21:57:02 +0800

    Add @fontsource/inter dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
