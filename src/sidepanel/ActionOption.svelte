e45ff7de26b7a0e4c68f604fd24e4b3d698018e9
Jerrylum <serverofjerry@gmail.com>
2024-06-08 22:34:02 +0800

    Remove classnames dep

--- src/sidepanel/ActionOption.svelte
+++ src/sidepanel/ActionOption.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import classNames from 'classnames'
   import { Action } from '../types'
 
   export let title: string
@@ -10,12 +9,9 @@
 <button
   {title}
   on:click={() => (setAction = representAction)}
-  class={classNames(
-    'hover:font-[500] cursor-pointer after:content-[attr(title)] after:block after:font-bold after:h-0 after:overflow-hidden after:invisible outline-none group',
-    {
-      '!font-bold underline': setAction === representAction,
-    },
-  )}
+  class="hover:font-[500] cursor-pointer after:content-[attr(title)] after:block after:font-bold after:h-0 after:overflow-hidden after:invisible outline-none group"
+  class:!font-bold={setAction === representAction}
+  class:underline={setAction === representAction}
   ><span
     class="outline-none outline-offset-2 group-focus-visible:outline-gray-400 group-focus-visible:outline-1"
     >{title}</span
